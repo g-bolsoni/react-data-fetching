a9902ae4c4466f049179c6094af9f35a8168e0fd
g-bolsoni <giovanebolsoni2@gmail.com>
2022-02-13 23:10:36 -0300

    Sem react-query

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect,  useState } from "react";
+import { useFetch } from "./hooks/useFecth";
+
+
+type Repository ={
+  full_name: string;
+  description:string;
+}
+function App() {
+
+  const {
+    data : repositories,
+    isFetching
+  } = useFetch<Repository[]>('users/g-bolsoni/repos')
+  return (
+    <ul>
+      {isFetching && <p> Carregando ....</p>}
+      {repositories?.map(repo =>{
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default App;
